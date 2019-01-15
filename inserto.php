e0ca7ac91d4d5e8b4a510badacb641d14ad2dbc0
AffekeNommu <32345567+AffekeNommu@users.noreply.github.com>
2019-01-15 23:30:25 +1100

    Create inserto.php
    Initial

---
+++ inserto.php
@@ -0,0 +1,22 @@
+<?php
+    $servername = "localhost";
+    $username = "username";
+    $password = "password";
+    $dbname = "Shopping";
+    $data=array();
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname, 0, '/path/to/mysql/socket');
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    $Item=$_GET["Item"];
+    $Shop=$_GET["Shop"];
+    $sql = "INSERT INTO List (Item, Shop) VALUES ('$Item','$Shop')";
+    if ($conn->query($sql) === TRUE) {
+        header ("location: https://path/to/index.php");
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+    $conn->close();
+    ?>
