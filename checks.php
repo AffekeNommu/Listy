f891171a7a8a96fe8b719ce9cda1eb39db90f099
AffekeNommu <32345567+AffekeNommu@users.noreply.github.com>
2019-01-27 08:23:28 +1100

    Update checks.php
    Removed pass and site refs. Oops.

--- checks.php
+++ checks.php
@@ -1,11 +1,11 @@
 <?php
     $servername = "localhost";
-    $username = "Listy";
-    $password = "L15tyU53r";
+    $username = "user";
+    $password = "password";
     $dbname = "Shopping";
     $data=array();
     //Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname, 0, '/media/zds/klvn8r/private/mysql/socket');
+    $conn = new mysqli($servername, $username, $password, $dbname, 0, '/path/to/mysql/socket');
     //check connection
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
@@ -21,14 +21,14 @@
     //nothing to change
     if ($checkbox[0]==''){
         $conn->close();
-        header ("location: https://klvn8r.polybotes.feralhosting.com/list/index.php");
+        header ("location: https://path/to/index.php");
     }
     //Walk through array and set the check flag on the items that were ticked
     for ($i = 0; $i <count($checkbox);$i++){
         $item= $checkbox[$i];
         $sql="update List set Checked=TRUE where Item ='$item'";
         if ($conn->query($sql) === TRUE) {
-            header ("location: https://klvn8r.polybotes.feralhosting.com/list/index.php");
+            header ("location: https://path/to/index.php");
         } else {
             echo "Error: " . $sql . "<br>" . $conn->error;
         }
